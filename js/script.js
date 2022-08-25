bab2e8d8e9184393e2e9831bbbdb5247a840b88e
Daniel <daniel.jimenezfrancisco@gmail.com>
2022-08-24 22:29:30 -0500

    Nav menu options are now collapsible.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 let navMenu = document.querySelector('.nav-menu')
 let hamburgerOpen = document.getElementById('hamburger-open')
 let hamburgerClose = document.getElementById('hamburger-close')
@@ -16,6 +17,36 @@ hamburgerClose.addEventListener("click", () =>
     hamburgerOpen.classList.toggle('active')
     hamburgerClose.classList.toggle('active')
     document.body.removeAttribute('style')
+})
+
+let button = document.querySelectorAll('[id=menu-header]')
+let submenuOptions = document.querySelectorAll('[id=submenu-option]')
+
+submenuOptions.forEach(function(div)
+{
+    console.log(div)
+    div.style.setProperty('--height', `${50 * div.children.length}px`)
+})
+
+button.forEach(function(btn, btnIndex)
+{
+    btn.addEventListener("click", () => 
+    {
+        submenuOptions.forEach(function(div, divIndex)
+        {
+            if (divIndex == btnIndex)
+            {
+                div.classList.toggle('active')
+            }
+            else
+            {
+                if (div.classList.contains('active'))
+                {
+                    div.classList.toggle('active')
+                }
+            }
+        })
+    })
 })
 
 
